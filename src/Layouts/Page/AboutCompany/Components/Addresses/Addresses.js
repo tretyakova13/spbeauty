80874167d5553447145feb126b77291b2faa9e50
tretyakova13 <124432765+tretyakova13@users.noreply.github.com>
2023-02-11 15:47:56 +0000

    nDesign01

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,11 +3,12 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4 p-4 col'>
+        <div className='d-inline-flex flex-column col borderAdress px-4 py-4 justify-content-between'>
             <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
+            <div className='d-flex flex-column gap-5 py-5'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+            <div/>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
         </div>
